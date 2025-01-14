8e7902204ff44a00d252772a217186cb1888e8e1
Riana-Alexia Cocoțian <riana.cocotian@student.upt.ro>
2025-01-14 11:12:30 +0200

    functie delete order adaugata(cel putin inceputa)

--- ProiectPoo/Orders/OrdersManager.cs
+++ ProiectPoo/Orders/OrdersManager.cs
@@ -1,31 +1,51 @@
-﻿    public class OrdersManager
+﻿using Microsoft.VisualBasic.ApplicationServices;
+
+public class OrdersManager
     {
         private Queue<Order> orders = new Queue<Order>();
         public void AddOrder(Order order)
         {
             orders.Enqueue(order);
         }
-        public bool DeleteOrder(int id)
+    public bool DeleteOrder(int id)
+    {
+        Order orderToDelete = null;
+        var ordersNewList = new Queue<Order>();
+        while (orders.Count > 0)
         {
-            Order orderToDelete = null;
-            var ordersNewList = new Queue<Order>();
-            while (orders.Count > 0)
+            var CurrentOrder = orders.Dequeue();
+            if (CurrentOrder.ID == id)
             {
-                var CurrentOrder = orders.Dequeue();
-                if (CurrentOrder.ID == id)
-                {
-                    orderToDelete = CurrentOrder;
-                }
-                else
-                {
-                    ordersNewList.Enqueue(CurrentOrder);
-                }
+                orderToDelete = CurrentOrder;
             }
-            orders = ordersNewList;
-            if (orderToDelete != null)
+            else
             {
-                return true;
+                ordersNewList.Enqueue(CurrentOrder);
             }
-            else return false;
+        }
+        orders = ordersNewList;
+        if (orderToDelete != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
+    public Order GetOrder()
+    {
+        var order = orders.Dequeue();
+        return new Order(order.ID, order.ObjectName, order.ObjectWeight, order.ObjectColour, order.PaymentSum, order.DeliveryAdress, order.Email, order.PhoneNumber);
+    }
+    public void DeleteOrder(User user)
+    {
+        var ordersNewList=new Queue<Order>();
+        while(orders.Count > 0)
+        {
+            var currentOrder = orders.Dequeue();
+            //cand termina Ana de completat
+        }
+    }
+}
+
