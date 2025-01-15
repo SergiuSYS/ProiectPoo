75af56e1d6b2a71dccd66fbada1002d1f82d5e34
Sergiu . <sergiusay06@gmail.com>
2025-01-15 09:14:32 +0200

    imprimanta

--- ProiectPoo/Imprimante/Imprimanta_base_model.cs
+++ ProiectPoo/Imprimante/Imprimanta_base_model.cs
@@ -1,26 +1,67 @@
-﻿public abstract class Imprimanta_base_model
+﻿public enum ImprimantaType
 {
+    FDM,
+    SLA
+}
+public class Imprimanta
+{
+    public int ID { get; set; }
     public string Brand { get; set; }
     public string Model { get; set; }
-    public float CapacitateMaterial { get; set; }
+    public Material material { get; set; }
     public float ProcentMaterialRamas { get; set; }
+    public ImprimantaType TipImprimanta { get; set; }
+    public bool Stare { get; set; }
+    private Order comanda;
 
-    public Imprimanta_base_model(string brand, string model, float capacitateMaterial, float procentMaterialRamas)
+    public Imprimanta(string brand, string model)
     {
         Brand = brand;
         Model = model;
-        CapacitateMaterial = capacitateMaterial;
         ProcentMaterialRamas = 100;
     }
     public float CalculareProcentMaterialRamas(float materialFolosit)
     {
-        if(materialFolosit > CapacitateMaterial)
+        if (materialFolosit > material.capacitateMaterial)
         {
             throw new Exception("Materialul folosit este mai mare decat capacitatea imprimantei");
         }
-        ProcentMaterialRamas = (CapacitateMaterial - materialFolosit) / CapacitateMaterial * 100;
+        ProcentMaterialRamas = (material.capacitateMaterial - materialFolosit) / material.capacitateMaterial * 100;
         return ProcentMaterialRamas;
     }
-    //se va introduce obiectuul comanda in viitor in loc de string model
-    public abstract bool ImprimareModel3D(string model);
+    public double TimpProcesare()
+    {
+        if (TipImprimanta == ImprimantaType.FDM)
+        {
+            return comanda.ObjectWeight * 1000;
+        }
+        if (TipImprimanta == ImprimantaType.SLA)
+        {
+            return comanda.ObjectWeight * 2000;
+        }
+        throw new Exception("Tipul imprimantei nu este valid.");
+    }
+    public void refil()
+    {
+
+    }
+    public void getComanda(Order order)
+    {
+        comanda = order;
+    }
+    public bool canPrint()
+    {
+
+        return true;
+    }
+    public async Task<string> PrintareComanda()
+    {
+        if (comanda == null)
+        {
+            throw new Exception("Nu exista nicio comanda de imprimat.");
+        }
+
+        await Task.Delay((int)TimpProcesare());
+        return "Comanda a fost printata";
+    }
 }
