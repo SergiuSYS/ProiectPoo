31a6995c091f42774f16c79b7164a45f7f5d79dd
Ana <anamaria.cioclei123@yahoo.com>
2025-01-15 13:12:57 +0200

    user

---
+++ ProiectPoo/User/ManagerUser.cs
@@ -0,0 +1,68 @@
+﻿
+class ManagerUser
+{
+    private List<User> userList = new List<User>();
+    private int userIdCounter = 1;
+
+    public void Register(string userName, string password)
+    {
+        User newUser = new User(userIdCounter++, userName, password);
+        userList.Add(newUser);
+        Console.Writeline($"User'{userName}' registered successfully'.");
+    }
+    public bool Login(string userName, string password)
+    {
+        foreach (var user in userList)
+        {
+            if (user.UserName == userName && user.Password == password)
+            {
+                Console.Writeline($"Login successful for user '{userName}'.");
+                return true;
+            }
+        }
+        Console.WriteLine("Invalide username or password.");
+        return false;
+    }
+
+    public void AddAccount(string userName, string password, boll isAdmin)
+    {
+        User newUser = new User(userIdCounter++, userName, password, isAdmin);
+        userList.Add(newUser);
+        Console.WriteLine($"Admin added user '{userName}' successfully.");
+    }
+
+    public void DeleteAccount(int userId)
+    {
+        var user = uerList.Find(u => u.ID == userId);
+        if (user != null)
+        {
+            ClearPendingOrders(userId);
+            userList.Remove(user);
+            Console.WriteLine($"User '{user.UserName}' deleted successfully.");
+        }
+        else
+        {
+            Console.WriteLine("User not found.");
+        }
+    }
+
+    public void ModifyAccount(int userId, string newUserName, string newPassword)
+    {
+        var user = uerList.Find(u => u.ID == userId);
+        if (user != null)
+        {
+            user.UserName = newUserName;
+            user.Password = newPassword;
+            Console.WriteLine($"User '{userId}' updated successfully.");
+        }
+        else
+        {
+            Console.WriteLine("User not found.");
+        }
+    }
+
+    private void ClearPendingOrders(int userId)
+    {
+        Console.WriteLine($"Clearing pending orders for user ID: {userId}...");
+    }
+}
---
+++ ProiectPoo/User/User.cs
@@ -0,0 +1,16 @@
+﻿class User
+{
+    public int ID { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public bool IsAdmin { get; set; } = false;
+
+    public User(int id, string userName, string password, bool isAdmin = false)
+    {
+        ID = id;
+        UserName = userName;
+        Password = password;
+        IsAdmin = isAdmin;
+    }
+}
+
